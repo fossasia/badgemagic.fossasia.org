331fa789909bc2544d69f0994e4376ee9c72ccc1
Planet Interactive <Planet Interactive@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-24 11:09:58 +0000

    First Commit v1.0 of Simple Twitter Tweets
    git-svn-id: https://plugins.svn.wordpress.org/simple-twitter-tweets/trunk@730791 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ README.txt
@@ -0,0 +1,124 @@
+=== Plugin Name ===
+Contributors: Planet Interactive
+Donate: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=A9437VN7R36VN
+Tags: Twitter, Stream, Tweets, Twitter OAuth
+Requires at least: 3.0 or Higher
+Tested up to: 3.5.1
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Simple Twitter Tweets - Display your Tweets on your Wordpress site using the new OAuth API v1.1. Because it works!
+
+== Description ==
+
+A Simple Twitter Tweets display widget, using Twitter OAth and API v1.1
+
+**Why?**
+
+Our clients, especially on shared hosting were having issues with the Twitter API not displaying their tweets, too many connections from the same source (host). We solved that issue, then lo and behold Twitter changed their API so displaying your own Tweets required OAuth authentication and finally we buckled and decided to roll our own so it would be simple for them, for us and for you.
+
+Twitter changed their API again. Removing version 1.0 of the API altogether and by forcing version 1.1 of the API and use of the OAuth authentication requirement. We wrote this plugin so everyone could have it at a click of a button.
+
+There are a few Twitter plugins out their, but we couldn't find one simple enough, or that worked (to be honest) and so the Simple Twitter Tweets plugin was born.
+
+Twitter users who want to display their Tweets on their website (or within an application) have to create a Twitter Application to get access to the required "Keys" and "Tokens" Twitter provides for Authentication. The instructions for this are provided below so you can be up and running with Tweets on your site in less time than it takes to make a cup of Tea.
+
+= Features =
+
+* Simple setup
+* Twitter API v1.1 compliant (OAuth Ready)
+* No passwords required or used
+* Works even when Twitters down, over capacity or not connecting
+* Tweets stored in the database with each call, so if your call to the Twitter API fails for whatever reason there won't be a blank space or Oops message on your site, you'll just see the last set of Tweets the system stored - sweet huh.
+* Tweeted when? - In Human Time using minutes, hours and days (i.e. 5 hours ago)
+* Did we say it was simple and works...
+
+== Installation ==
+
+Installation is as simple as 1,2,3 or maybe 4 because of Twitter :)
+
+1. Upload `simple-twitter-tweets` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Add the Widget to your page (e.g. Sidebar) and update the required details
+4. Note: You will need to create a Twitter Application - See below. Oh, it's really easy.
+
+= Creating a Twitter Application =
+
+The Twitter Widget will never ask for your password, as it gets all your required data from the Open Authentication keys and secrets you will get by creating your application at Twitter. It also means that if you change your password, you won’t need to update any of the details of your Widget.
+
+To find these details, go to https://dev.twitter.com/ and sign in.
+
+Once you have logged in successfully, hover over your name in the top right corner, and click "My Applications," then "Create a New Application."
+
+Enter a unique name (anything you want), a description (again this is just for you), and your site's URL. You can leave the Callback URL empty as it is not used for this implementation.
+
+Yay, success - OK! You will be taken to a new screen, there's one more step then you can copy all the details into correct fields of the Widget and be on your way.
+
+OK, click the "Create my Access Token" button. This is a shortcut to authenticate your own account with your application (so you never need use your password).
+
+Good. Now click the Details Tab as all the information you need is presented hereso you can just copy the required information into the exact corresponding inputs fields of the Widget.
+
+Full details and screenshots of this process can seen on the [Simple Twitter Tweets page](http://www.planet-interactive.co.uk/simple-twitter-tweets "Simple Twitter Tweets page by Planet Interactive")
+
+= The Widget Options =
+
+Fill in your details, copy and past the Twitter Application details (as described below).
+
+* You can select the Title of the Widget as you like.
+* Enter your Twitter username (without the @) just the name.
+* How many Tweets to display
+* The time in minutes between updates (per the API requirement) this should be 5 but if the API changes you can alter it here.
+* Consumer Key: Under the *OAuth settings* heading
+* Consumer Secret: Under the *OAuth settings* heading
+* Access Token: Under the *Your access token* heading
+* Access Token Secret: Under the *Your access token* heading
+* Choose if you want the @replies included or not
+* Click Save
+
+Enjoy!
+
+== Frequently Asked Questions ==
+
+= Can I change the look and feel of the Tweets =
+
+Of course you can. It's really simple too.
+
+The Tweets are in a widget as with all widgets, and are a simple unordered list.
+
+* To make styling easier the <ul> has a class of Tweets - <ul class="tweets">
+* Each Tweet is a list item <li>
+* Each Time reference is an emphasised link <em><a>
+* Done.
+
+= Where can I get help =
+
+If you're really stuck check out the [support portal](http://planetinteractive.freshdesk.com/support/login "Support by Planet Interactive")
+
+= More FAQs =
+As far as we know it just works! Phew, but if you have an issue or you want to propose some functionality then submit you ideas at the [support portal](http://planetinteractive.freshdesk.com/support/login "Support by Planet Interactive") and we'll update these FAQs and get onto it when we can.
+
+== Screenshots ==
+
+Setup your Twitter Application
+1. Go to https://dev.twitter.com and Sign In
+2. Top right, hover your name/icon, go to My Application
+3. Create a new Application
+4. Fill a name for your App, a description (this is for you) and your website address (URL)
+5. Click "Create my access token"
+6. If you've already installed in the "Simple Twitter Tweets" plugin go to Appearance->Widgets (otherwise install it first then go here)
+7. Drag the "Simple Twitter Tweets" widget your widget area of choice
+8. Fill in the widget options and correlating Twitter Application OAuth requirements, just copy and paste
+9. Style how you like, "Your Tweets your way"
+
+See here [Simple Twitter Tweets](http://www.planet-interactive.co.uk/simple-twitter-tweets "Simple Twitter Tweets by Planet Interactive")
+
+== Changelog ==
+
+= 1.0 =
+* Initial release
+
+== Upgrade Notice ==
+
+= 1.0 =
+This is the initial release using the new Twitter OAuth requirement to pull Tweets
---
+++ simple-twitter-tweets.php
@@ -0,0 +1,259 @@
+<?php
+/*
+Plugin Name: Simple Twitter Tweets
+Plugin URI: http://www.planet-interactive.co.uk/simple-twitter-tweets
+Description: Display last x number tweets from Twitter API stream, store locally in dataabse to present past tweets when failure to access Twitters restrictive API occurs
+Author: Ashley Sheinwald
+Version: 1.0
+Author URI: http://www.planet-interactive.co.uk/
+*/
+
+/*  Copyright 2013  Ashley Sheinwald  (email : ashley@planet-interactive.co.uk)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+// USED FOR DEBUG
+// include 'console.php';
+
+class PI_SimpleTwitterTweets extends WP_Widget{
+
+	function PI_SimpleTwitterTweets()  {
+		$widget_ops = array('classname' => 'PI_SimpleTwitterTweets', 'description' => 'Displays the most recent tweets from your Twitter Stream' );
+		//$control_ops = array( 'width' => 300, 'height' => 350, 'id_base' => 'Not-required-this-time' );  
+		$this->WP_Widget('PI_SimpleTwitterTweets', 'Simple Twitter Tweets', $widget_ops);
+	}
+ 
+	function form($instance){
+		
+		//Set up some default widget settings.
+		$defaults = array( 
+			  'title' 			=> __('Recent Tweets', 'pi-tweet')
+			, 'name' 			=> __('iPlanetUK', 'pi-tweet')
+			, 'numTweets' 		=> __(4, 'pi-tweet') // How many to display
+			, 'cacheTime' 		=> __(5, 'pi-tweet') // Time in minutes between updates
+			, 'consumerKey' 		=> __('xxxxxxxxxxxx', 'pi-tweet') // Consumer key
+			, 'consumerSecret' 		=> __('xxxxxxxxxxxx', 'pi-tweet') // Consumer secret
+			, 'accessToken' 		=> __('xxxxxxxxxxxx', 'pi-tweet') // Access token
+			, 'accessTokenSecret'	=> __('xxxxxxxxxxxx', 'pi-tweet') // Access token secret
+			, 'exclude_replies'	=> true 
+		);
+		$instance 			= wp_parse_args( (array) $instance, $defaults );
+		$title 				= $instance['title'];
+		$name 				= $instance['name'];
+		$numTweets 			= $instance['numTweets'];
+		$cacheTime 			= $instance['cacheTime'];
+		$consumerKey 		= $instance['consumerKey'];
+		$consumerSecret 	= $instance['consumerSecret'];
+		$accessToken 		= $instance['accessToken'];
+		$accessTokenSecret 	= $instance['accessTokenSecret'];
+		$exclude_replies 	= $instance['exclude_replies'];
+?>
+		<p>
+			<label for="<?php echo $this->get_field_id('title'); ?>">Title: <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo attribute_escape($title); ?>" /></label>
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id('name'); ?>">Twitter Name: <input class="widefat" id="<?php echo $this->get_field_id('name'); ?>" name="<?php echo $this->get_field_name('name'); ?>" type="text" value="<?php echo attribute_escape($name); ?>" /></label>
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id('numTweets'); ?>">Number of Tweets: <input class="widefat" id="<?php echo $this->get_field_id('numTweets'); ?>" name="<?php echo $this->get_field_name('numTweets'); ?>" type="text" value="<?php echo attribute_escape($numTweets); ?>" /></label>
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id('cacheTime'); ?>">Time in Minutes between updates: <input class="widefat" id="<?php echo $this->get_field_id('cacheTime'); ?>" name="<?php echo $this->get_field_name('cacheTime'); ?>" type="text" value="<?php echo attribute_escape($cacheTime); ?>" /></label>
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id('consumerKey'); ?>">Consumer Key: <input class="widefat" id="<?php echo $this->get_field_id('consumerKey'); ?>" name="<?php echo $this->get_field_name('consumerKey'); ?>" type="text" value="<?php echo attribute_escape($consumerKey); ?>" /></label>
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id('consumerSecret'); ?>">Consumer Secret: <input class="widefat" id="<?php echo $this->get_field_id('consumerSecret'); ?>" name="<?php echo $this->get_field_name('consumerSecret'); ?>" type="text" value="<?php echo attribute_escape($consumerSecret); ?>" /></label>
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id('accessToken'); ?>">Access Token: <input class="widefat" id="<?php echo $this->get_field_id('accessToken'); ?>" name="<?php echo $this->get_field_name('accessToken'); ?>" type="text" value="<?php echo attribute_escape($accessToken); ?>" /></label>
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id('accessTokenSecret'); ?>">Access Token Secret: <input class="widefat" id="<?php echo $this->get_field_id('accessTokenSecret'); ?>" name="<?php echo $this->get_field_name('accessTokenSecret'); ?>" type="text" value="<?php echo attribute_escape($accessTokenSecret); ?>" /></label>
+		</p>
+		<p>  
+		    <input class="checkbox" type="checkbox" <?php checked( isset( $instance['exclude_replies']), true ); ?> id="<?php echo $this->get_field_id( 'exclude_replies' ); ?>" name="<?php echo $this->get_field_name( 'exclude_replies' ); ?>" />   
+		    <label for="<?php echo $this->get_field_id( 'exclude_replies' ); ?>"><?php _e('Exclude @replies', 'pi-tweet'); ?></label>  
+		</p>
+	<?php
+	}
+
+	function update($new_instance, $old_instance){
+		$instance = $old_instance;
+		
+	    //Strip tags from title and name to remove HTML  
+	    $instance['title'] 				= strip_tags( $new_instance['title'] );  
+	    $instance['name'] 				= strip_tags( $new_instance['name'] );  
+	    $instance['numTweets'] 			= $new_instance['numTweets'];
+	    $instance['cacheTime'] 			= $new_instance['cacheTime'];
+	    $instance['consumerKey'] 		= $new_instance['consumerKey'];
+	    $instance['consumerSecret'] 	= $new_instance['consumerSecret'];
+	    $instance['accessToken'] 		= $new_instance['accessToken'];
+	    $instance['accessTokenSecret'] 	= $new_instance['accessTokenSecret'];
+	    $instance['exclude_replies'] 	= $new_instance['exclude_replies'];
+	
+		return $instance;
+	}
+ 
+	function widget($args, $instance){
+		extract($args, EXTR_SKIP);
+
+		echo $before_widget;
+
+		//Our variables from the widget settings.
+		$PI_title = empty($instance['title']) ? ' ' : apply_filters('widget_title', $instance['title']);
+		$PI_name = $instance['name'];
+		$PI_numTweets = $instance['numTweets'];
+		$PI_cacheTime = $instance['cacheTime'];
+
+		//Setup Twitter API OAuth tokens
+		$PI_consumerKey = $instance['consumerKey'];
+		$PI_consumerSecret = $instance['consumerSecret'];
+		$PI_accessToken = $instance['accessToken'];
+		$PI_accessTokenSecret = $instance['accessTokenSecret'];
+
+		$PI_exclude_replies = isset( $instance['exclude_replies'] ) ? $instance['exclude_replies'] : false;
+
+
+		if (!empty($PI_title))
+			echo $before_title . $PI_title . $after_title;;
+
+			// START WIDGET CODE HERE
+			?>
+
+			<ul class="tweets">
+			<?php
+			/*
+	 		 * Uses:
+			 * Twitter API call:
+			 *     http://dev.twitter.com/doc/get/statuses/user_timeline
+			 * WP transient API ref.
+			 *		http://www.problogdesign.com/wordpress/use-the-transients-api-to-list-the-latest-commenter/
+			 * Plugin Development and Script enhancement
+			 *    http://www.planet-interactive.co.uk
+			 */
+
+			// Configuration.
+			$numTweets 			= $PI_numTweets; 	// Num tweets to show
+			$name 				= $PI_name;				// Twitter UserName
+			$cacheTime 			= $PI_cacheTime; 		// Time in minutes between updates.
+
+			// Get from https://dev.twitter.com/
+			// Login - Create New Application, fill in details and use required data below
+			$consumerKey 		= $PI_consumerKey;		// OAuth Key
+			$consumerSecret 	= $PI_consumerSecret;	// OAuth Secret
+			$accessToken 		= $PI_accessToken;		// OAuth Access Token
+			$accessTokenSecret 	= $PI_accessTokenSecret;// OAuth Token Secret
+
+			$exclude_replies 	= $PI_exclude_replies; // Leave out @replies?
+
+			$transName = 'list-tweets'; // Name of value in database.
+			$backupName = $transName . '-backup'; // Name of backup value in database.
+
+			// Do we already have saved tweet data? If not, lets get it.
+			if(false === ($tweets = get_transient($transName) ) ) :    
+
+			// Get the tweets from Twitter.
+			include 'twitteroauth/twitteroauth.php';
+			  
+			$connection = new TwitterOAuth(
+				$consumerKey,   		// Consumer key
+				$consumerSecret,   	// Consumer secret
+				$accessToken,   		// Access token
+				$accessTokenSecret	// Access token secret
+			);
+
+			// If excluding replies, we need to fetch more than requested as the
+			// total is fetched first, and then replies removed.
+			$totalToFetch = ($excludeReplies) ? max(50, $numTweets * 3) : $numTweets;
+			
+			$fetchedTweets = $connection->get(
+				'statuses/user_timeline',
+				array(
+					'screen_name'     => $name,
+					'count'           => $totalToFetch,
+					'exclude_replies' => $excludeReplies
+				)
+			);
+			  
+			// Did the fetch fail?
+			if($connection->http_code != 200) :
+				$tweets = get_option($backupName); // False if there has never been data saved.
+			    
+			else :
+				// Fetch succeeded.
+				// Now update the array to store just what we need.
+				// (Done here instead of PHP doing this for every page load)
+				$limitToDisplay = min($numTweets, count($fetchedTweets));
+			
+				for($i = 0; $i < $limitToDisplay; $i++) :
+					$tweet = $fetchedTweets[$i];
+			    
+			    	// Core info.
+			    	$name = $tweet->user->name;
+			    	$permalink = 'http://twitter.com/'. $name .'/status/'. $tweet->id_str;
+
+			    	/* Alternative image sizes method: http://dev.twitter.com/doc/get/users/profile_image/:screen_name */
+			    	$image = $tweet->user->profile_image_url;
+
+			    	// Message. Convert links to real links.
+			    	$pattern = '/http:(\S)+/';
+			    	$replace = '<a href="${0}" target="_blank" rel="nofollow">${0}</a>';
+			    	$text = preg_replace($pattern, $replace, $tweet->text);
+
+			    	// Need to get time in Unix format.
+			    	$time = $tweet->created_at;
+			    	$time = date_parse($time);
+			    	$uTime = mktime($time['hour'], $time['minute'], $time['second'], $time['month'], $time['day'], $time['year']);
+
+			    	// Now make the new array.
+			    	$tweets[] = array(
+			    		'text' => $text,
+			    		'name' => $name,
+			    		'permalink' => $permalink,
+			    		'image' => $image,
+			    		'time' => $uTime
+			    		);
+				endfor;
+
+				// Save our new transient, and update the backup.
+				set_transient($transName, $tweets, 60 * $cacheTime);
+				update_option($backupName, $tweets);
+				endif;
+			endif;
+
+			// Now display the tweets, if we can.
+			if($tweets) : ?>
+			    <?php foreach($tweets as $t) : ?>
+			        <li><?php echo $t['text']; ?>
+			            <br/><em>
+			            <a href="http://www.twitter.com/<?php echo $name; ?>" target="_blank" title="Follow <?php echo $name; ?> on Twitter [Opens new window]"><?php echo human_time_diff($t['time'], current_time('timestamp')); ?> ago</a>
+			            </em>
+			        </li>
+			    <?php endforeach; ?>
+
+			<?php else : ?>
+			    <li>Waiting for Twitter... Once Twitter is ready they will display my Tweets again.</li>
+			<?php endif; ?>
+			</ul>
+			<?php
+			// END OF WIDGET CODE HERE
+			echo $after_widget;
+		}
+ 
+}
+add_action( 'widgets_init', create_function('', 'return register_widget("PI_SimpleTwitterTweets");') );
+?>
