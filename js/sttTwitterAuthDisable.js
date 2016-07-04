dc6e851beb4a890f2c48dc46fc230aa84f064741
Sopan <khoslasopan@gmail.com>
2016-07-05 00:05:03 +0530

    Adds plugin initials as prefix for script functions

--- js/sttTwitterAuthDisable.js
+++ js/sttTwitterAuthDisable.js
@@ -1,18 +1,18 @@
 jQuery(document).ready(function ($) {
 	if($(".loklakAPI").prop('checked')) {
-		wpltf_update_twitter_auth(true);
+		stt_update_twitter_auth(true);
 	}
 
-    $(".loklakAPI").live('change', function() {
-    	if($(this).is(':checked')){
-	    	wpltf_update_twitter_auth(true);
+	$(".loklakAPI").live('change', function() {
+		if($(this).is(':checked')){
+	    	stt_update_twitter_auth(true);
 	    }
 	    else {
-	    	wpltf_update_twitter_auth(false);
+	    	stt_update_twitter_auth(false);
 	    }
 	});
 
-	function wpltf_update_twitter_auth(arg) {
+	function stt_update_twitter_auth(arg) {
 		if (arg == true) {
 	    	$(".consumerKey").prop('disabled', arg);
 			$(".consumerSecret").prop('disabled', arg);
